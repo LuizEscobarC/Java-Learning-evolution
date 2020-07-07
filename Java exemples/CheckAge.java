085e1f47f6c2029acb675dcf123fbef93033344f
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-06 21:38:40 -0400

    Java Exercises
    learning

---
+++ Java exemples/CheckAge.java
@@ -0,0 +1,13 @@
+public class CheckAge {
+    public static void main (String[] args) {
+        int idade = 15;
+        //estrutura de decisão
+        if (idade < 18){
+            System.out.println("It connot gonna into");
+        }
+        else{
+            System.out.println("Open to you");
+        }    
+        System.exit(0);
+    }
+}
