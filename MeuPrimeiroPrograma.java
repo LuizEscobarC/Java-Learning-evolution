f4d6db3d72d81e4d88a694e0130bfd1aca030487
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-06 20:45:32 -0400

    FirstProgramJava
    Java First Program

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+public class MeuPrimeiroPrograma{
+    public static void main ( String args [] )
+    {
+        System.out.println("Meu Primeiro programa em JAVA");
+        System.exit (0);
+    }
+} 
